1cfc82098b3d1d722df150a3efa1234058da2183
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-06-24 21:16:32 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,57 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
+
+    def title #attr_reader
+        @title
+    end
+
+    def author=(author) #attr_accessor
+        @author = author
+    end
+
+    def author #attr_accessor
+        @author
+    end
+
+    def page_count=(num) #attr_accessor
+        @page_count = num
+    end
+
+    def page_count #attr_accessor
+        @page_count
+    end
+
+    def genre=(genre) #attr_accessor
+        @genre = genre
+    end
+
+    def genre #attr_accessor
+        @genre
+    end
+
+    def turn_page
+        @turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
+
+#SHORTER VERSION
+# class Book
+#     attr_accessor :author, :page_count, :genre
+#     attr_reader :title
+   
+#     def initialize(title)
+#       @title = title
+#     end
+   
+#     def turn_page
+#       puts "Flipping the page...wow, you read fast!"
+#     end
+   
+#   end
 
 
